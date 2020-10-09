b02bdb26a9c65ff3df90cdc88c3c09ab6733287d
crickschmelz <44099598+crickschmelz@users.noreply.github.com>
2020-10-08 20:00:49 -0700

    Merged changes

--- src/components/CreateAccount/CreateAccount.test.js
+++ src/components/CreateAccount/CreateAccount.test.js
@@ -1,8 +1,16 @@
-import React from "react";
+/*import React from "react";
 import CreateAccount from "./";
 import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
   const tree = renderer.create(<CreateAccount />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
 });
--- src/components/Listings/Listings.test.js
+++ src/components/Listings/Listings.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Listings from "./";
-import renderer from "react-test-renderer";
+import { renderer } from "react-test-renderer";
 
 it("renders correctly", () => {
   const tree = renderer.create(<Listings />).toJSON();
